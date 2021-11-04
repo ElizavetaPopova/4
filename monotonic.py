32543a5eb4ba8e6f5b31d22b2c979760a6db68b4
ElizavetaPopova <91835543+ElizavetaPopova@users.noreply.github.com>
2021-11-04 02:12:31 +0200

    Update monotonic.py

--- monotonic.py
+++ monotonic.py
@@ -1,20 +1,20 @@
-print("Введите элементы списка в строку:")
+print("Введите элементы списка в строчку:")
 num = input()
 nums = num.split()
 nums = list(map(int, nums))
-n = len(nums)
+f = len(nums)
 print('nums =', nums)
 def is_monotonic(nums):
-    i = 1
+    k = 1
     counter = 0
-    for i in range(1,n):
+    for k in range(1,f):
         if min(nums) == nums[0]:
-            if nums[i] >= nums[i-1]:
+            if nums[k] >= nums[k-1]:
                 continue
             else:
                 counter += 1
         elif max(nums) == nums[0]:
-            if nums[i] <= nums[i-1]:
+            if nums[k] <= nums[k-1]:
                 continue
             else:
                 counter += 1
